28329ce5db3c7285b8537947affdcafa452ff62e
BrenoMaia98 <brenomaia98@gmail.com>
2019-10-20 01:10:49 -0300

    Adicionadas as fontes ao projeto com as seus devidos nomes siplificados e Weigths para cada uma delas, Finalizado também a estilização da navBar e da imagem de fundo da página inicial.

--- Site/sitehumanus/src/Componentes/NavBar/NavBar.js
+++ Site/sitehumanus/src/Componentes/NavBar/NavBar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './style.css'
-import logo from '../../imagens/LogoCompleto.png'
+
     
 export default function NavBar(){
 
     return(
         <div className="fundo">
-            <img src={logo} className="logo"/>
             <ul className="menu">
                 <li>HOME</li>
                 <li>EMPRESA</li>
