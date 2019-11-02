3a7310e2624da3cacc8f47ee835fde981e3a4ecd
Loren <loren_alonso21@hotmail.com>
2019-11-02 03:17:23 -0300

    Modificação das fontes - QuemSomos/Contato/Footer

--- Site/sitehumanus/src/App.js
+++ Site/sitehumanus/src/App.js
@@ -5,6 +5,8 @@ import FundoLogo from './imagens/FundoLogoCompleto.png';
 
 import QuemSomos from './Componentes/QuemSomos/index'
 import Servicos from './Componentes/Servico/index'
+import Footer from "./Componentes/Footer/index";
+import CardSP from "./Componentes/CardSP/index";
 
 function App() {
   return (
@@ -17,7 +19,7 @@ function App() {
       <Servicos></Servicos>
       <div className="container">
            <div className="content"> 
-              <p><strong>Contato</strong></p>
+              <p>CONTATO</p>
                 <form> 
                   <p><label htmlfor="nome">Nome: </label>
                   <input type="nome" id="nome" placeholder="Nome completo"/></p>
@@ -25,12 +27,33 @@ function App() {
                   <input type="email" id="email" placeholder="Seu melhor email"/></p>
                   <p><label htmlfor="comentario">Assunto: </label>
                   <input type="comentario" id="comentario" placeholder="Faça seu comentário"/></p>
-                  <button className="btn" type="submit">Enviar</button>
+                  <button className="btn" type="submit"><strong>Enviar</strong></button>
                 </form>
             </div>
       </div>
+      <div style={{"display":"flex"}}>
+        <div style={{ "width": "20%", "marginLeft":"auto" }}>
+          <p style={{"textAlign":"center","marginBottom":"10px","color":"#212b56" ,"fontSize":"25px","marginRight":"40%","font-family": "Bebas"}}>Localização:</p>
+          <p style={{"marginBottom":"7px","color":"#212b56","font-family": "CapitalAlegreya","font-weight":"normal","font-style":"normal"}}> Av Dom Antonio, n°2100, Assis CPPA </p>
+            <p style={{"marginBottom":"7px","color":"#212b56","font-family": "CapitalAlegreya","font-weight":"normal","font-style":"normal"}}> CEP: 19806-173</p>
+            <p style={{"marginBottom":"7px","color":"#212b56","font-family": "CapitalAlegreya","font-weight":"normal","font-style":"normal"}}>Tel: (18) 3302-5915</p>
+          
+        </div>
 
-
+      <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.152824389586!2d-50.43616008549995!3d-22.64808998514402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9495397733797ebf%3A0xa09bb8fb9160d747!2sAv.%20Dom%20Ant%C3%B4nio%20-%20Jardim%20Parana%2C%20Assis%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1571864835766!5m2!1spt-BR!2sbr"
+          style={{
+            "border": "0px",
+            "width": "50%",
+            "height":"50%",
+            
+            "marginRight":"auto",
+            "marginBottom":"10px"
+          }}
+          allowfullscreen=""
+        ></iframe>
+      </div>
+      <Footer></Footer>
 
 
 
---
+++ Site/sitehumanus/src/Componentes/Footer/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './style.css'
+import WhatsApp2 from '../../imagens/WhatsApp2.png'
+import Twitter2 from '../../imagens/Twitter2.png'
+import Facebook2 from '../../imagens/Facebook2.png'
+import instagram2 from '../../imagens/instagram2.png'
+
+export default function Footer(){
+return(
+<div class="container body-content">
+    
+    <footer class="footer">
+        <div>
+        <ul className="Icones">
+        <li><a href ="https://www.whatsapp.com/"target="_blank"><img src={WhatsApp2} className="WhatsApp"/> </a></li>
+        <li><a href ="https://www.instagram.com/humanus_ej/?hl=pt-br"target="_blank"><img src={instagram2} className="instagram"/> </a></li>
+        <li><a href ="https://twitter.com/humanusjr"target="_blank"><img src={Twitter2} className="Twitter"/></a> </li>
+        <li><a href ="https://www.facebook.com/HumanusEJ/?__tn__=%2Cd%2CP-R&eid=ARBhe7mEjvAeXMNEZNds0zW-o9R92IWueMVWQh6s2-Ij2FVxYUR3x-GGQc8LlOmV-bDR3R7Q1okOFaF2"target="_blank"><img src={Facebook2} className="Facebook"/></a></li>
+        </ul>
+        </div>
+        <div className="Texto">
+        
+        <p style={{"marginBottom":"10px","margin-right": "115px"}}>Tel:(18) 3302-5915</p>
+        <p style={{"marginBottom":"10px","margin-right": "120px"}}>CEP: 19806-173</p>
+        <p style={{"marginBottom":"10px", "margin-right": "50px"}}>Av Dom Antonio, n°2100, Assis (CPPA)</p>
+        
+        <div className="Texto2">
+        <p>Desenvolvido por EJCOMP- @ Empresa Junior de Computação</p>
+        </div>
+        </div>
+
+    </footer>
+</div>
+);
+}
