58209c72df3b915b1652b40fa65c6ca0b9868a85
BrenoMaia98 <brenomaia98@gmail.com>
2019-11-17 11:50:03 -0300

    adicionado todo o blog, e seus componentes

--- Site/sitehumanus/src/App.js
+++ Site/sitehumanus/src/App.js
@@ -1,47 +1,12 @@
 import React from 'react';
-import './App.css';
-import NavBar from './Componentes/NavBar/NavBar';
-import FundoLogo from './imagens/FundoLogoCompleto.png';
-
-import QuemSomos from './Componentes/QuemSomos/index'
-import Servicos from './Componentes/Servico/index'
+import Principal from './Paginas/Principal/principal'
+import Blog from './Paginas/Blog/blog'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <div>
-        <img src={FundoLogo} className="fundoLogo"/>
-      </div>
-      <QuemSomos></QuemSomos>
-      <Servicos></Servicos>
-      <div className="container">
-           <div className="content"> 
-              <p><strong>Contato</strong></p>
-                <form> 
-                  <p><label htmlfor="nome">Nome: </label>
-                  <input type="nome" id="nome" placeholder="Nome completo"/></p>
-                  <p><label htmlfor="email">E-mail: </label>
-                  <input type="email" id="email" placeholder="Seu melhor email"/></p>
-                  <p><label htmlfor="comentario">Assunto: </label>
-                  <input type="comentario" id="comentario" placeholder="Faça seu comentário"/></p>
-                  <button className="btn" type="submit">Enviar</button>
-                </form>
-            </div>
-      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-    </div>
+    <>
+   <Blog></Blog>
+    </>
   );
 }
 
---
+++ Site/sitehumanus/src/Paginas/Principal/principal.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./principal.css";
+import NavBar from "../../Componentes/NavBar/NavBar";
+import FundoLogo from "../../imagens/FundoLogoCompleto.png";
+import QuemSomos from "../../Componentes/QuemSomos/index";
+import Servicos from "../../Componentes/Servico/index";
+
+function Principal() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <div>
+        <img src={FundoLogo} className="fundoLogo" />
+      </div>
+      <QuemSomos></QuemSomos>
+      <Servicos></Servicos>
+      <div className="container">
+        <div className="content">
+          <p>
+            <strong>Contato</strong>
+          </p>
+          <form>
+            <p>
+              <label htmlfor="nome">Nome: </label>
+              <input type="nome" id="nome" placeholder="Nome completo" />
+            </p>
+            <p>
+              <label htmlfor="email">E-mail: </label>
+              <input type="email" id="email" placeholder="Seu melhor email" />
+            </p>
+            <p>
+              <label htmlfor="comentario">Assunto: </label>
+              <input
+                type="comentario"
+                id="comentario"
+                placeholder="Faça seu comentário"
+              />
+            </p>
+            <button className="btn" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Principal;
+
+/*
+      
+*/
